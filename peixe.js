eed15b1a9a1843b4a2640f32b030aeaa2ef69a79
Yuri <123526190+B0czar@users.noreply.github.com>
2025-02-05 22:09:51 -0300

    Update background and logo image also adjust logo scaling in game

--- peixe.js
+++ peixe.js
@@ -21,39 +21,40 @@ var game = new Phaser.Game(config);
 var Logo;
 var Sushi;
 var Musica;
+var Mar;
 
 //funcition that preloads the visual elements of the text
 function preload() {
     this.load.audio("musica", "assets/babyshark.mp3"); //pre load musica de fundo
-
-    this.load.image("mar", "assets/bg/bg_azul-claro.png"); //preload do background
-
-    //this.load.image("mar", "assets/bg/bg_azul-escuro.png"); //
-
+    this.load.image("mar", "assets/bg/bg_azul-claro.jpg"); //preload do background
     this.load.image("sushi", "assets/peixes/tubarao.png"); //preload do peixe
-
-    //this.load.image("logo", "assets/bg/logo-inteli_branco.png"); //preload do logo do Inteli
-
-    this.load.image("logo", "assets/bg/logo-inteli_azul.png"); //o branco ficou ruim.
-
-    this.load.image("yuri", "assets/bg/Yuri Boczar.png"); //preload do meu nome
+    this.load.image("logo", "assets/bg/logo.svg"); //o branco ficou ruim.
 }
 
 //function that adds the elements into the game
 function create() {
-    this.add.image(400, 300, "mar"); //add mar nas coord 400, 300
+    // Create a tile sprite for the background that repeats to cover the entire screen
+    Mar = this.add.tileSprite(
+        0,
+        0,
+        window.innerWidth,
+        window.innerHeight,
+        "mar"
+    );
+    Mar.setOrigin(0, 0);
+    Mar.setTint(0xaaaaaa);
 
     //cria a variavel Logo e seta ela como a imagem na esq inferior do bg
-    Logo = this.add.image(100, 550, "logo"); //quis fazer de forma diferente do tutorial
-    Logo.setScale(0.5); //achei muito grande o logo.
-
-    this.add.image(250, 575, "yuri").setScale(0.3); //add meu nome do lado do logo do Inteli
+    Logo = this.add.image(
+        window.innerWidth - 100,
+        window.innerHeight - 550,
+        "logo"
+    ); //quis fazer de forma diferente do tutorial
+    Logo.setScale(0.15); //achei muito grande o logo.
 
     //peixe
     Sushi = this.add.image(400, 300, "sushi");
-    Sushi.setOrigin(0.5, 0.5); //origem: meio da tela
     Sushi.setScale(0.5); //tava muito grande
-
     //musica
     Musica = this.sound.add("musica"); //add audio
     Musica.play({ loop: true }); //da play em loop
@@ -63,5 +64,6 @@ function update() {
     Sushi.x = this.input.x;
     Sushi.y = this.input.y;
 }
-
 //Yuri Boczar - GitHub: B0czar
+
+//notas para mim: preciso colocar essa margem preta que tem em volta da barrinha de som no logo para ficar mais legivel.
